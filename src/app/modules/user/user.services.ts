e30602adaae1d72e78d314064a99f004bc970451
SafiaJotey <safiaahmed1786@gmail.com>
2023-08-12 19:49:28 +0600

    Cors issuen

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -2,14 +2,22 @@ import ApiError from '../../../error/ApiError'
 import { IUser } from './user.interface'
 import { User } from './user.model'
 
-const createUser = async (book: IUser): Promise<IUser | null> => {
-  const newUser = await User.create(book)
+const createUser = async (user: IUser): Promise<IUser | null> => {
+  if (user?.email === 'OPTIONS') {
+    // Handle CORS pre-flight request
+    return null; // Return an empty response
+  }
+  const newUser = await User.create(user)
   if (!newUser) {
     throw new ApiError(440, 'Failed to create new user')
   }
   return newUser
 }
 const getUser = async (email: string): Promise<IUser | null> => {
+  if (email === 'OPTIONS') {
+    // Handle CORS pre-flight request
+    return null; // Return an empty response
+  }
   const user = await User.findOne({ email: email })
 
   return user
