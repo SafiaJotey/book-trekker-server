8e442cac73ae25e21d7f0963478cd46f06dd21d8
SafiaJotey <safiaahmed1786@gmail.com>
2023-08-12 21:26:04 +0600

    cors issue

--- dist/app.js
+++ dist/app.js
@@ -35,10 +35,7 @@ const reading_route_1 = require("./app/modules/reading/reading.route");
 const user_route_1 = require("./app/modules/user/user.route");
 const wishlist_route_1 = require("./app/modules/wishlist/wishlist.route");
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)({
-    origin: '*',
-    credentials: true,
-}));
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use((0, express_1.urlencoded)({ extended: true }));
 app.use('/', express_1.default.static('upload'));
