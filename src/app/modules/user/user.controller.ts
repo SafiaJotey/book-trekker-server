6689d03aba67592931b878682dae75c606c0b1d2
SafiaJotey <safiaahmed1786@gmail.com>
2023-08-12 21:39:27 +0600

    cors issue

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -6,12 +6,7 @@ import { UserServices } from './user.services'
 //create an user
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    // if (req.method === 'OPTIONS') {
-    //   // Handle CORS pre-flight request
-    //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    //   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    //   return res.sendStatus(200);
-    // }
+
     const { ...user } = req.body
 
     const newUser = await UserServices.createUser(user)
@@ -29,12 +24,7 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
 const getUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    // if (req.method === 'OPTIONS') {
-    //   // Handle CORS pre-flight request
-    //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    //   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    //   return res.sendStatus(200);
-    // }
+
     const user = await UserServices.getUser(req.params.email)
     sendResponse<IUser>(res, {
       statusCode: 200,
--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -3,10 +3,6 @@ import { IUser } from './user.interface'
 import { User } from './user.model'
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
-  // if (user?.email === 'OPTIONS') {
-  //   // Handle CORS pre-flight request
-  //   return null; // Return an empty response
-  // }
   const newUser = await User.create(user)
   if (!newUser) {
     throw new ApiError(440, 'Failed to create new user')
@@ -14,10 +10,6 @@ const createUser = async (user: IUser): Promise<IUser | null> => {
   return newUser
 }
 const getUser = async (email: string): Promise<IUser | null> => {
-  // if (email === 'OPTIONS') {
-  //   // Handle CORS pre-flight request
-  //   return null; // Return an empty response
-  // }
   const user = await User.findOne({ email: email })
 
   return user
